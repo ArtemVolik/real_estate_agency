23db5748fc51a9714498e9c803c05acf23450fd7
artyom <art.volyk@gmail.com>
2020-12-04 11:49:40 +0200

    add admin filter

--- property/admin.py
+++ property/admin.py
@@ -11,5 +11,6 @@ class AdminFlat(admin.ModelAdmin):
     list_display = ['owner', 'address', 'owners_phonenumber',
                    'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
 admin.site.register(Flat, AdminFlat)
